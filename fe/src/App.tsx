33d734d17195197ecde19155a13aa578582ac03a
chithong2k2 <20127634@student.hcmus.edu.vn>
2023-12-20 13:54:39 +0700

    Hoàn thành xem chi tiết cấp phép chỉnh sửa bảng qc

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdLicense from "pages/admin/AdLicense";
 import EditAdLicense from "pages/admin/EditAdLicense";
 import EditAdLocationLicenseDetail from "pages/admin/EditAdLicense/components/EditAdLocationLicense/EditAdLocationLicenseDetail";
+import EditAdTableLicenseDetail from "pages/admin/EditAdLicense/components/EditAdTableLicense/EditAdTableLicenseDetail";
 
 function App() {
   return (
@@ -14,11 +15,15 @@ function App() {
         <Route path="/admin" Component={UserManagement} />
         <Route path="/" Component={MapAdsManagement} />
         <Route path="/admin/review/edit" Component={EditAdLicense} />
-
         <Route
-          path="/admin/review/edit/:id"
+          path="/admin/review/edit/location/:id"
           Component={EditAdLocationLicenseDetail}
         />
+        <Route
+          path="/admin/review/edit/table/:id"
+          Component={EditAdTableLicenseDetail}
+        />
+
         <Route path="/admin/review/license" Component={AdLicense} />
       </Routes>
     </BrowserRouter>
