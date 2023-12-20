51bc718a3000a28de5d29e1ba3f66df8693af8a6
Gia Tien Truong <80575488+TG-Tienes@users.noreply.github.com>
2023-12-20 19:23:32 +0700

    create contract form (#3)
    * Ad detail in contract form
    
    * Ad detail in contract form
    
    * Basic create contract form
    
    Not done dropzone for image
    
    * Upload image, datepicker form
    
    upload image to website and upload to cloudinary
    Get sign date and end date of contract from datepicker field using react hook form
    
    * Fix form helpertext location error
    
    * Done create contract form
    
    * Fix styles for contract form, add Id to sections of form
    
    * fix form structure
    
    merge form title and section nav bar to form details
    adjust color of form detail content
    
    * Add yup to create contract form

---
+++ fe/src/pages/admin/CreateContractForm/components/AdvertiseDetails/index.tsx
@@ -0,0 +1,94 @@
+import { Box, Card, Divider, Icon } from "@mui/material";
+import React from "react";
+import Heading6 from "../../../../../components/common/text/Heading6";
+import ParagraphBody from "../../../../../components/common/text/ParagraphBody";
+import ParagraphSmall from "../../../../../components/common/text/ParagraphSmall";
+import images from "config/images";
+import classes from "./style.module.scss";
+
+import clsx from "clsx";
+import FormTopTab from "../FormTopTab";
+
+interface advertiseDetailProp {
+  adType: string;
+  address: string;
+  width: number;
+  height: number;
+  quantity: number;
+  adForm: string;
+  locationType: string;
+}
+
+function AdDetails(adDetail: advertiseDetailProp) {
+  return (
+    <Card>
+      <Box className={classes.topGroup}>
+        <h1>Tạo cấp phép mới</h1>
+        <FormTopTab />
+      </Box>
+
+      <Box className={clsx(classes.detailContainer)}>
+        <Box className={classes.detailGroup}>
+          <Heading6 id="general" fontSize={"20px"} fontWeight={500}>
+            {adDetail.adType}
+          </Heading6>
+          <Heading6 fontWeight={50}>{adDetail.address}</Heading6>
+        </Box>
+        <Divider className={classes.divider} variant="middle" />
+        <Box className={classes.detailGroup}>
+          <Heading6 id="details">Thông tin chi tiết bảng quảng cáo</Heading6>
+          <Box className={classes.detailItem}>
+            <div>
+              <Heading6 fontWeight={100}>
+                <img src={images.sizeIcon} className={classes.iconItem} />
+                Kích thước
+              </Heading6>
+            </div>
+            <Heading6 fontWeight={800}>
+              {adDetail.width}m x {adDetail.height}m
+            </Heading6>
+          </Box>
+          <Box className={classes.detailItem}>
+            <div>
+              <Heading6 fontWeight={100}>
+                <img src={images.quantityIcon} className={classes.iconItem} />
+                Số lượng
+              </Heading6>
+            </div>
+            <Heading6 fontWeight={800}>{adDetail.quantity} trụ / bảng</Heading6>
+          </Box>
+          <Box className={classes.detailItem}>
+            <div>
+              <Heading6 fontWeight={100}>
+                <img
+                  src={images.categoryIcon}
+                  className={clsx(classes.iconItem)}
+                />
+                Hình thức
+              </Heading6>
+            </div>
+            <Heading6 fontWeight={800}>{adDetail.adForm}</Heading6>
+          </Box>
+          <Box className={classes.detailItem}>
+            <div>
+              <Heading6 fontWeight={100}>
+                <img src={images.sortIcon} className={classes.iconItem} />
+                Phân loại
+              </Heading6>
+            </div>
+            <Heading6 fontWeight={800}>{adDetail.locationType}</Heading6>
+          </Box>
+        </Box>
+        <Divider className={classes.divider} variant="middle" />
+
+        <Box className={classes.detailGroup}>
+          <Heading6 id="mapLocation">
+            Vị trí đặt bảng quảng cáo trên bản đồ
+          </Heading6>
+        </Box>
+      </Box>
+    </Card>
+  );
+}
+
+export default AdDetails;
