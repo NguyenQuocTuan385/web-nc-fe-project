7dac60ad26099c57357b62cdf90dc02bb96dd2f7
chithong2k2 <20127634@student.hcmus.edu.vn>
2023-12-21 10:21:36 +0700

    xu ly conflict

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -5,31 +5,36 @@ import UserManagement from "./pages/admin/UserManagement";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdLicense from "pages/admin/AdLicense";
 import EditAdLicense from "pages/admin/EditAdLicense";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import ContractForm from "pages/admin/CreateContractForm";
 import EditAdLocationLicenseDetail from "pages/admin/EditAdLicense/components/EditAdLocationLicense/EditAdLocationLicenseDetail";
 import EditAdTableLicenseDetail from "pages/admin/EditAdLicense/components/EditAdTableLicense/EditAdTableLicenseDetail";
 import AdLicenseDetail from "pages/admin/AdLicense/components/AdLicenseDetail";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin" Component={UserManagement} />
-        <Route path="/" Component={MapAdsManagement} />
-        <Route path="/admin/review/edit" Component={EditAdLicense} />
-        <Route
-          path="/admin/review/edit/location/:id"
-          Component={EditAdLocationLicenseDetail}
-        />
-        <Route
-          path="/admin/review/edit/table/:id"
-          Component={EditAdTableLicenseDetail}
-        />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/admin" Component={UserManagement} />
+          <Route path="/" Component={MapAdsManagement} />
+          <Route path="/admin/review/edit" Component={EditAdLicense} />
 
-        <Route path="/admin/review/license" Component={AdLicense} />
-
-        <Route path="/admin/review/license/:id" Component={AdLicenseDetail} />
-      </Routes>
-    </BrowserRouter>
+          <Route
+            path="/admin/review/edit/location/:id"
+            Component={EditAdLocationLicenseDetail}
+          />
+          <Route
+            path="/admin/review/edit/table/:id"
+            Component={EditAdTableLicenseDetail}
+          />
+          <Route path="/admin/review/license" Component={AdLicense} />
+          <Route path="/admin/review/license/:id" Component={AdLicenseDetail} />
+          <Route path="/contracts/createForm" element={<ContractForm />} />
+        </Routes>
+      </BrowserRouter>
+    </LocalizationProvider>
   );
 }
 
