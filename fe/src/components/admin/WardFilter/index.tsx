4d506a0f78672f6fe588024987602194db75bae0
Gia Tien Truong <truongtien577@gmail.com>
2023-12-26 11:08:39 +0700

    Get ward filter query string

--- fe/src/components/admin/WardFilter/index.tsx
+++ fe/src/components/admin/WardFilter/index.tsx
@@ -5,6 +5,9 @@ import { Box, Button, Card, FormControl, FormControlLabel, FormGroup, FormLabel 
 import Heading6 from "components/common/text/Heading6";
 import { Property } from "models/property";
 import Checkbox from "@mui/material/Checkbox";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+import { check } from "prettier";
 
 interface PropsData {
   propertyList?: Property[];
@@ -12,13 +15,35 @@ interface PropsData {
 }
 
 function WardFilter(propsData: PropsData) {
-  const [checkbox, setCheckbox] = useState([]);
-  const [filteredList, setFilteredList] = useState<string[]>([]);
+  const locationHook = useLocation();
+  const wardParams = queryString.parse(locationHook.search).wardFilter;
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const [filteredList, setFilteredList] = useState<string[]>(() => {
+    const initFilteredArray: string[] = [];
+    propsData.propertyList?.forEach((item, index) => {
+      if (wardParams?.includes(index.toString())) initFilteredArray.push(item.id.toString());
+    });
+
+    return initFilteredArray;
+  });
+
+  const [checkbox, setCheckbox] = useState(() => {
+    const indexArray: boolean[] = [];
+
+    propsData.propertyList?.forEach((item, index) => {
+      wardParams?.includes(index.toString()) ? (indexArray[index] = true) : (indexArray[index] = false);
+    });
+
+    return indexArray;
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>, checkIndex: number) => {
     event.target.checked
       ? setFilteredList([...filteredList, event.target.name])
       : setFilteredList(filteredList.filter((item) => item !== event.target.name));
+
+    setCheckbox(checkbox.map((item, index) => (index === checkIndex ? (item = event.target.checked) : item)));
+    console.log(checkbox);
   };
 
   const sendFilterHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -38,7 +63,13 @@ function WardFilter(propsData: PropsData) {
           {propsData.propertyList?.map((propertyItem, index) => (
             <FormControlLabel
               key={propertyItem.id}
-              control={<Checkbox name={propertyItem.id.toString()} checked={checkbox[index]} onChange={handleChange} />}
+              control={
+                <Checkbox
+                  name={propertyItem.id.toString()}
+                  checked={checkbox[index]}
+                  onChange={(e) => handleChange(e, index)}
+                />
+              }
               label={propertyItem.name}
             />
           ))}
