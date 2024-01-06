afe8109bd3e2f75786a55590c287334799f76930
Gia Tien Truong <truongtien577@gmail.com>
2024-01-07 06:18:57 +0700

    Fix and add some features to login form
    Add react hook form with yup to Login page form
    Add grid item of Grid MUI

--- fe/src/pages/admin/Login/index.tsx
+++ fe/src/pages/admin/Login/index.tsx
@@ -1,47 +1,80 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
 import classes from "./styles.module.scss";
 import picture from "../../../assets/img/login/login.jpg";
 import { routes } from "routes/routes";
 import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface FormData {
+  email: string;
+  password: string;
+}
+
 const Login: React.FC = () => {
   const navigate = useNavigate();
-
-  const handleLogin = () => {
-    navigate(routes.admin.properties.district);
+  const handleLogin = (data: any) => {
+    console.log(data);
+    // navigate(routes.admin.properties.district);
   };
+  const schema = useMemo(() => {
+    return yup.object().shape({
+      email: yup.string().required("Vui lòng điền email"),
+      password: yup.string().required("Vui lòng điền mật khẩu")
+    });
+  }, []);
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>({ resolver: yupResolver(schema) });
+
   return (
     <Box className={classes.loginContainer}>
       <Grid container spacing={2} className={classes.formBox}>
-        <Grid xs={6} className={classes.imageContainer}>
+        <Grid item xs={6} className={classes.imageContainer}>
           <img src={picture} alt='login' className={classes.imageContainer} />
         </Grid>
 
-        <Grid xs={6} className={classes.formContainer}>
+        <Grid item xs={6} className={classes.formContainer}>
           <Typography variant='h5' gutterBottom>
             Đăng nhập
           </Typography>
-          <TextField
-            label='Username'
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            className={classes.customTextField}
-          />
-          <TextField
-            label='Password'
-            type='password'
-            variant='outlined'
-            margin='normal'
-            fullWidth
-            required
-            className={classes.customTextField}
-          />
 
-          <Button onClick={handleLogin} variant='contained' size='large' fullWidth className={classes.loginButton}>
-            Đăng nhập
-          </Button>
+          <form onSubmit={handleSubmit(handleLogin)}>
+            <TextField
+              label='Email'
+              variant='outlined'
+              margin='normal'
+              required
+              fullWidth
+              {...register("email")}
+              className={classes.customTextField}
+            />
+            <TextField
+              label='Password'
+              type='password'
+              variant='outlined'
+              margin='normal'
+              fullWidth
+              required
+              {...register("password")}
+              className={classes.customTextField}
+            />
+
+            <Button
+              variant='contained'
+              type='submit'
+              size='large'
+              fullWidth
+              className={classes.loginButton}
+            >
+              Đăng nhập
+            </Button>
+          </form>
 
           <Link href='#' className={classes.forgotPasswordLink}>
             Quên mật khẩu
