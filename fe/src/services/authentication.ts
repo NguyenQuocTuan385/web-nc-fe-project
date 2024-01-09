0c8d4cf9e96d7dc02feffab36092c3a90226bd34
Dương Chí Thông <20127634@student.hcmus.edu.vn>
2024-01-09 07:57:29 +0700

    Cap nhat lai tao tai khoan thanh dang ky tai khoan

--- fe/src/services/authentication.ts
+++ fe/src/services/authentication.ts
@@ -1,11 +1,23 @@
 import { LoginRequest } from "models/authentication";
 import { apiAuth } from "./configApi";
 import { API } from "config/constant";
+import { GetUsers, UserRequest } from "models/user";
 
 export class AuthenticationService {
   static async login(data: LoginRequest): Promise<any> {
     return await apiAuth
       .post(`${API.AUTH.LOGIN}`, data, { withCredentials: true })
+      .then((res) => {
+        return Promise.resolve(res.data);
+      })
+      .catch((e) => {
+        return Promise.reject(e?.response?.data);
+      });
+  }
+
+  static async register(data: UserRequest): Promise<any> {
+    return await apiAuth
+      .post(`${API.AUTH.REGISTER}`, data, { withCredentials: true })
       .then((res) => {
         return Promise.resolve(res.data);
       })
