2993a2c0995605e03f05d5515b6158f07940fc89
Gia Tien Truong <truongtien577@gmail.com>
2024-01-13 11:05:58 +0700

    Fix contract UI and contract table logic

--- fe/src/pages/common/UnAuthorized/index.tsx
+++ fe/src/pages/common/UnAuthorized/index.tsx
@@ -3,8 +3,14 @@ import Heading3 from "components/common/text/Heading3";
 import Heading4 from "components/common/text/Heading4";
 import images from "config/images";
 import classes from "./styles.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const UnAuthorized = () => {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
     <Box className={classes.boxWrap}>
       <Box className={classes.unauthorizedBox}>
@@ -12,7 +18,7 @@ const UnAuthorized = () => {
       </Box>
       <Heading3>Dừng lại</Heading3>
       <Heading4>Bạn không có quyền truy cập vào trang này</Heading4>
-      <Button variant='contained' sx={{ mt: 2, width: 200, height: 50 }}>
+      <Button onClick={handleBack} variant='contained' sx={{ mt: 2, width: 200, height: 50 }}>
         Quay về
       </Button>
     </Box>
